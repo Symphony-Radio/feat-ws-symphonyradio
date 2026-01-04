c10d4f81d5ec8117daf63d76267b943ad717f70a
Braiden Hill <141950732+braidenhxyz@users.noreply.github.com>
2026-01-04 11:00:20 -0600

    Add Symphony Radio API documentation
    Added documentation for Symphony Radio APIs and streams.

---
+++ src/connectors/symphonyradio.ts
@@ -0,0 +1,5 @@
+// Symphony Radio APIs and Streams Links
+// Stream: https://stream.symphonyradio.co.uk/
+// API Stats: https://panel.symphradio.live/api/stats
+// API Last Played: https://panel.symphradio.live/api/recentlyplayed
+// Coded and Added by: Braiden Hill, Station Director of Symphony Radio
